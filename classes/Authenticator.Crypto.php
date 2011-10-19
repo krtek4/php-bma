ddf2457b2f7a130e46c98cdad94ff663d81bfdde
Gilles Meier <krtek4@gmail.com>
2011-10-19 19:48:57 +0000

    initial commit

---
+++ classes/Authenticator.Crypto.php
@@ -0,0 +1,42 @@
+<?php
+
+// Some crypto functions for Blizzard Authenticator 
+// RSA part based on http://stevish.com/rsa-encryption-in-pure-php 
+
+// This script is distributed under the terms of the GNU General Public License (GPL)
+// See http://www.gnu.org/licenses/gpl.txt for license details
+
+class Authenticator_Crypto {
+	static private $rsa_mod = "104890018807986556874007710914205443157030159668034197186125678960287470894290830530618284943118405110896322835449099433232093151168250152146023319326491587651685252774820340995950744075665455681760652136576493028733914892166700899109836291180881063097461175643998356321993663868233366705340758102567742483097";
+	static private $rsa_exp = '257';
+	static private $keysize = 1024;
+
+	static public function encrypt($text) {
+		$text = self::bchexdec(bin2hex($text));
+		$n = bcpowmod($text, self::$rsa_exp, self::$rsa_mod);
+		$ret = '';
+		while($n > 0) {
+			$ret = chr(bcmod($n, 256)).$ret;
+			$n = bcdiv($n, 256, 0);
+		}
+		return $ret;
+	}
+
+	static public function decrypt($code, $key) {
+		$ret = '';
+		for($i = 0; $i < strlen($code); ++$i) {
+			$c = ord($code{$i});
+			$k = ord($key{$i});
+			$ret .= chr($c ^ $k);
+		}
+		return $ret;
+	}
+
+	static public function bchexdec($hex) {
+		$dec = 0;
+		$len = strlen($hex);
+		for ($i = 1; $i <= $len; $i++)
+			$dec = bcadd($dec, bcmul(strval(hexdec($hex[$i - 1])), bcpow('16', strval($len - $i))));
+		return $dec;
+	}
+}
---
+++ classes/Authenticator.php
@@ -0,0 +1,137 @@
+<?php
+
+require_once('Authenticator.Crypto.php');
+
+class AuthenticatorException extends Exception { }
+
+class Authenticator {
+	static private $server = 'm.%s.mobileservice.blizzard.com';
+	static private $initialize_uri = "/enrollment/enroll.htm";
+	static private $synchronize_uri = "/enrollment/time.htm";
+
+	private $region = null;
+	private $sync = null;
+	private $serial = null;
+	private $secret = null;
+
+	public function __construct($serial = null, $secret = null) {
+		if(! is_null($serial) && ! is_null($secret)) {
+			$this->region = substr($serial, 0, 2);
+			$this->secret = $secret;
+			$this->serial = $serial;
+		}
+	}
+
+	public function region() {
+		if(is_null($this->region))
+			throw new AuthenticatorException();
+		return $this->region;
+	}
+
+	public function servertime() {
+		if(is_null($this->sync))
+			$this->synchronize();
+		return (int) (microtime(true) * 1000) + $this->sync;
+	}
+
+	private function set_sync($server_time) {
+		$server_time = hexdec(bin2hex($server_time));
+		$current_time = (int) (microtime(true) * 1000);
+		$this->sync = $server_time - $current_time;
+	}
+
+	public function serial() {
+		if(is_null($this->serial))
+			throw new AuthenticatorException();
+		return $this->serial;
+	}
+
+	private function set_serial($serial) {
+		$this->serial = $serial;
+	}
+
+	public function secret() {
+		if(is_null($this->secret))
+			throw new AuthenticatorException();
+		return $this->secret;
+	}
+
+	private function set_secret($secret) {
+		$this->secret = bin2hex($secret);
+		var_dump($this->secret);
+	}
+
+	private function server() {
+		return sprintf(self::$server, strtolower($this->region()));
+	}
+
+	private function encrypt($data) {
+		return Authenticator_Crypto::encrypt($data);
+	}
+
+	private function decrypt($data, $key) {
+		return Authenticator_Crypto::decrypt($data, $key);
+	}
+
+	private function send($uri, $data = null) {
+		$host = $this->server();
+		$method = is_null($data) ? 'GET' : 'POST';
+		$data = is_null($data) ? '' : $this->encrypt($data);
+
+		$http = fsockopen($host, 80, $errno, $errstr, 2);
+		if($http) {
+			fputs($http, "$method $uri HTTP/1.1\r\n");
+			fputs($http, "Host: $host\r\n");
+			fputs($http, "Content-Type: application/octet-stream\r\n");
+			fputs($http, "Content-length: ".strlen($data)."\r\n");
+			fputs($http, "Connection: close\r\n\r\n");
+			fputs($http, $data);
+
+			$result = '';
+			while(! feof($http))
+				$result .= fgets($http, 128);
+		} else
+			throw new AuthenticatorException();
+		fclose($http);
+
+		$result = explode("\r\n\r\n", $result, 2);
+
+		return $result[1];
+	}
+
+	public function initialize($region = 'US') {
+		$f_code = chr(1);
+		$enc_key = substr(sha1(rand()), 0, 37);
+		$model = str_pad('PHP_BNA', 16, chr(0), STR_PAD_RIGHT);
+
+		$data = $f_code.$enc_key.$region.$model;
+		$response = $this->send(self::$initialize_uri, $data);
+
+		$this->set_sync(substr($response, 0, 8));
+		$data = $this->decrypt(substr($response, 8), $enc_key);
+		$this->set_secret(substr($data, 0, 20));
+		$this->set_serial(substr($data, 20));
+	}
+
+	public function synchronize() {
+		$response = $this->send(self::$synchronize_uri);
+		$this->set_sync($response);
+	}
+
+	public function code() {
+		// calculate current interval number
+		$intervalNumber = (int)($this->servertime() / 30000);
+		// calculate HMAC-SHA1 from secret key and interval number
+		$mac = hash_hmac('sha1', $intervalNumber, $this->secret());
+		// determine which 4 bytes of the MAC are taken as the current code
+		// last 4 bit of the MAC points to the starting byte
+		$startPos = hexdec($mac{39});
+		// select the byte at starting position and the following 3 bytes
+		$selectedInt = hexdec(substr($mac, $startPos, 8));
+		// use the lowest 8 decimal digits from the selected integer as the
+		// current authenticator code
+		return $selectedInt % 100000000;
+	}
+}
+
+?>
